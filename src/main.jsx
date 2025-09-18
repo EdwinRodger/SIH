d1c1c7ad7710f8c95060628e7620309a1d85af9f
EdwinRodger <codevaidik@gmail.com>
2025-09-18 13:59:00 +0530

    add scrolltotop to always be on top of the page

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import App from './App.jsx'
 import './i18n/index.js'
+import ScrollToTop from "./ScrollToTop"
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
+            <ScrollToTop />
             <App />
         </BrowserRouter>
     </StrictMode>,
